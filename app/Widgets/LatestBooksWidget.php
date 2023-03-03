0890d0ff1e000e2ea903f9ae489b1b8aa55e8cd7
Olivier Gorzalka <olivier@amphibee.fr>
2023-03-03 16:22:11 +0100

    Ajout de la gestion de l'affichage

--- app/Widgets/LatestBooksWidget.php
+++ app/Widgets/LatestBooksWidget.php
@@ -10,4 +10,28 @@ class LatestBooksWidget extends \WP_Widget
             'description' => 'Displays the latest books.',
         ]);
     }
+
+    public function form($instance)
+    {
+        $title = $instance['title'] ?? '';
+
+        echo view('admin.widgets.latest-books', [
+           'field_id' => $this->get_field_id('title'),
+           'field_name' => $this->get_field_name('title'),
+           'title' => $title,
+        ]);
+    }
+
+    public function update($new_instance, $old_instance)
+    {
+        $instance['title'] = strip_tags($new_instance['title']);
+        return $instance;
+    }
+
+    public function widget($args, $instance)
+    {
+        echo view('front.widgets.latest-books', [
+            'title' => $instance['title'],
+        ]);
+    }
 }
