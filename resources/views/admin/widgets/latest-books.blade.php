bf5220fb702ecd038ad6cfc22548f4082466db4b
Olivier Gorzalka <olivier@amphibee.fr>
2023-03-03 16:10:01 +0100

    Ajout de la vue du widget

---
+++ resources/views/admin/widgets/latest-books.blade.php
@@ -0,0 +1,4 @@
+<p>
+    <label for="{{ $field_id }}"><?php _e( 'Title' ); ?></label>
+    <input class="widefat" id="{{ $field_id }}" name="{{ $field_name }}" type="text" value="{{ $title }}">
+</p>
